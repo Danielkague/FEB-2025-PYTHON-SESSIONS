fe20817249be8c186b7dbbb14f8a26f5e5a46d82
Danielkague <daniel.kague@gmail.com>
2025-03-14 15:29:57 +0300

    python variables

--- WEEK 1/index.py
+++ WEEK 1/index.py
@@ -1,1 +1,31 @@
 print("Hello world")
+
+number = 10
+print(number)
+
+
+
+
+
+age = 27 # Int Datatype
+marks = 90.5 # Float Datatype
+
+name = "Daniel Kague" #string DataType
+print(age)
+print(marks)
+print(name)
+
+
+
+
+
+# Lists Datatypes
+
+languages = ["python", "HTML", "CSS", 3, 5.7, True]
+print(languages[-6])
+print(languages[0])
+# Define the variable
+academy_name = "PLP Academy"
+
+# Print the variable
+print(academy_name)
