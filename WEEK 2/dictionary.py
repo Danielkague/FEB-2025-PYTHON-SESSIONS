d241b37c8509f0763107110409967b0a9092981f
Danielkague <daniel.kague@gmail.com>
2025-03-25 10:21:40 +0300

    initial commit

---
+++ WEEK 2/dictionary.py
@@ -0,0 +1,18 @@
+students = {
+    "name": "evans",
+    "age": 24,
+    "gender": "male",
+    "is_student": True,
+    "courses": ["Math", "science", "English"],
+
+    "my_name" : {
+        "first_name": "evans",
+        "last_name": "kinyua"
+    }
+}
+
+students["height"] = 2.56
+
+print(students)
+print(students["age"])
+#print(students["courses"])
---
+++ WEEK 2/sets.py
@@ -0,0 +1,2 @@
+students_ids = {1, 2, 3, 4, True, "john"}
+print(students_ids)
